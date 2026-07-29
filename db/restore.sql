f3af620f32d383c32322fc5e91cc851a8ed4ba26
Mouad El Baz <elbaz.ouad1249@gmail.com>
2026-07-29 20:34:52 +0100

    feat: restore file

---
+++ db/restore.sql
@@ -0,0 +1,16 @@
+USE master;
+GO
+
+RESTORE DATABASE AdventureWorks2022
+FROM DISK = '/var/opt/mssql/backup/AdventureWorks2022.bak'
+WITH
+    MOVE 'AdventureWorks2022'
+        TO '/var/opt/mssql/data/AdventureWorks2022.mdf',
+
+    MOVE 'AdventureWorks2022_log'
+        TO '/var/opt/mssql/data/AdventureWorks2022_log.ldf',
+
+    REPLACE,
+    RECOVERY,
+    STATS = 10;
+GO
