4db2a279579799522516bf195f6590f2616cb365
Mouad El Baz <elbaz.ouad1249@gmail.com>
2026-08-27 23:29:37 +0100

    feat: EXERCICES OF UNDERSTAND OREDER BY | DISTINCT | ALIAS | TOP

--- practice/02_orderby_distinct_top_alias.sql
+++ practice/02_orderby_distinct_top_alias.sql
@@ -33,3 +33,59 @@ SELECT
     Nom AS NomEmploye,
     Salaire AS SalaireMensuel
 FROM Employe;
+
+{# EXERCICES #}
+
+Exercice 11:
+SELECT *
+FROM Employe
+ORDER BY Salaire ASC;
+
+Exercice 12:
+SELECT *
+FROM Employe
+ORDER BY Salaire DESC;
+
+Exercice 13:
+SELECT Nom, Age
+FROM Employe
+ORDER BY Age DESC;
+
+Exercice 14:
+SELECT DISTINCT DepartementID
+FROM Employe;
+
+Exercice 15:
+SELECT DISTINCT DepartementID 
+FROM Employe
+WHERE DepartementID IS NOT null
+ORDER BY DepartementID ASC;
+
+Exercice 16:
+SELECT TOP 3 Nom, Salaire
+FROM Employe
+ORDER BY Salaire DESC;
+
+Exercice 17:
+SELECT TOP 1 Nom
+FROM Employe
+ORDER BY AGE ASC;
+
+Exercice 18:
+SELECT 
+	Nom AS NomEmploye,
+	Salaire AS SalaireMensuel
+FROM Employe
+ORDER BY SalaireMiensuel DESC;
+
+Exercice 19:
+SELECT Nom
+FROM Employe
+WHERE DepartementID = 2
+ORDER BY Age DESC;
+
+Exercice 20:
+SELECT *
+FROM Employe
+WHERE Salaire BETWEEN 7000 AND 11000
+ORDER BY Salaire DESC;
